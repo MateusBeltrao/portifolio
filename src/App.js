1592f5b8692b769635358ee6bcedd628dc543a00
mateus <mateusbeltrao2019@gmail.com>
2024-08-12 08:10:57 -0300

    add testimonial

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonias/Testimonials';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <About></About>
         <Skills></Skills>
         <Services></Services> 
-
+        <Testimonials></Testimonials>
       
        
       </main>
---
+++ src/components/testimonias/Data.jsx
@@ -0,0 +1,25 @@
+import Image1 from "../../assets/99freelas.png"
+import Image2 from "../../assets/testimonial2.png"
+import Image3 from "../../assets/testimonial3.png"
+
+const Data = [
+    {
+      id: 1,
+      image: Image1,
+      title: "Renan S.",
+      description:
+        "Uma pessoa muito boa e de fácil comunicação. Recomendo demais o Mateus, um cara muito gente fina, ágil e sempre disposto a ajudar. Ele realiza o trabalho com competência e rapidez, tornando todo o processo mais eficiente e agradável. Estou muito satisfeito com a colaboração e, sem dúvida, trabalharei com ele novamente.",
+    },
+    {
+      id: 2,
+      image: Image1,
+      title: "Anderson A.",
+      description:
+        "O Mateus fez um bom trabalho, gostei da entrega dele. Está iniciando ainda, mas é dedicado e atendeu todos meus pedidos a respeito do projeto.",
+    },
+   
+  ];
+
+
+
+  export default Data
